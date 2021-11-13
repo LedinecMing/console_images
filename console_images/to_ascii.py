dfd81d85c5a3c48e706a72570245287cd9c2bcef
Урядов Алексей <63245550+LedinecMing@users.noreply.github.com>
2021-11-14 00:46:13 +0400

    Add files via upload

--- console_images/to_ascii.py
+++ console_images/to_ascii.py
@@ -8,11 +8,11 @@ from symbol_table import SymbolTable
 from term import get_terminal_size, clear_term
 from palette import color_it_full
 
-__all__: typing.Sequence = ["generate_image",
-                            "print_image",
-                            "print_images",
-                            "show_gif",
-                            "TextImage"]
+__all__: typing.Sequence[str] = ["generate_image",
+                                 "print_image",
+                                 "print_images",
+                                 "show_gif",
+                                 "TextImage"]
 
 ascii_table: SymbolTable = SymbolTable([' ', '.', ':', "'",
                                         ',', '-', ';', '_',
@@ -57,17 +57,23 @@ def print_image(text_image: TextImage,
         print()
 
 
-def print_images(*text_images: typing.Sequence[TextImage],
-                 sep: str = ""):
-    image_generators: typing.Sequence[typing.Generator] = \
-        list(map(lambda t_image: generate_image(t_image), text_images))
-    max_size: int = \
-        max(list(map(lambda t_image: t_image.size[1], text_images)))
+def print_images(*text_images: TextImage,
+                 sep: str = "",
+                 color_function: typing.Callable[
+                     [typing.Tuple
+                      [int, int, int]], str] = color_it_full
+                 ) -> None:
+    image_generators: typing.Dict[typing.Generator[
+                                      str, None, None], TextImage] = \
+        {generate_image(t_image[1],
+                        color_function=color_function): t_image[1]
+         for t_image in enumerate(text_images)}
+    max_size: int = max([t_image.size[1] for t_image in text_images])
 
     for current_line in range(max_size):
-        print(*list(
-            map(lambda img_generator:
-                img_generator.__next__(), image_generators)),
+        print(*[img_generator.__next__()
+                for img_generator, t_image_index
+                in image_generators.items()],
               sep=sep)
 
 
@@ -75,7 +81,7 @@ def generate_image(text_image: TextImage,
                    color_function: typing.Callable[
                        [typing.Tuple
                         [int, int, int]], str] = color_it_full
-                   ) -> typing.Generator:
+                   ) -> typing.Generator[str, None, None]:
     """String generator of image"""
     image: Image = text_image.to_convertable()
     image_grayscale: Image.Image = image.convert("L")
@@ -103,7 +109,7 @@ def show_gif(path: str,
             text_image: TextImage = TextImage(image,
                                               (columns, rows),
                                               table=table)
-            image: Image = text_image.to_convertable()
+            image = text_image.to_convertable()
             image_grayscale: Image = image.convert("L")
             for i in range(0, image.height):
                 for j in range(0, image.width):
