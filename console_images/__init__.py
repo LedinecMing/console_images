5d26d6e96deab620ac1c9a56165c35e9e8e5ad96
Uriadov Aleksey <63245550+LedinecMing@users.noreply.github.com>
2021-11-18 08:28:38 +0400

    Update __init__.py

--- console_images/__init__.py
+++ console_images/__init__.py
@@ -18,9 +18,3 @@ limitations under the License.
 from .to_ascii import *
 from .palette import *
 from .term import get_terminal_size
-
-if __name__ == "__main__":
-    show_gifs("example.gif",
-              size=get_terminal_size(),
-              color_function=color_it_full,
-              dizering=True)
